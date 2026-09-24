c008e0511095336f9203c4abb15e02d4480a06a7
Toni Fernandez <toni.fernandez@cirvianum.cat>
2026-09-24 20:14:29 +0200

    language pas 1

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 //L'index ha de carregar el model i redirigir al home
 session_start();
+include('./config/config.php');
 include('./model/users.php');
 header('Location: ./views/home.php');
 exit;
---
+++ language/an.php
@@ -0,0 +1,4 @@
+<?php
+$text = [
+    'title_index' => 'Authentication users example'
+];
